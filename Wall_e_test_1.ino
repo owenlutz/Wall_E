2d99011346bd045c2c22c7346f3591f861606b69
owenlutz <122942905+owenlutz@users.noreply.github.com>
2023-04-20 15:48:17 -0700

    Update Wall_e_test_1.ino

--- Wall_e_test_1.ino
+++ Wall_e_test_1.ino
@@ -1,86 +1,84 @@
-// Pin definitions
-#define motor1Pin1 2
-#define motor1Pin2 3
-#define motor2Pin1 4
-#define motor2Pin2 5
+// Include the Arduino library
+#include <Arduino.h>
 
-// Motor speed
-#define motorSpeed 150
-//This is all in setup to only run once for a test
+// Define the pins for controlling the motors
+const int motorPin1 = 2;
+const int motorPin2 = 3;
+const int motorPin3 = 4;
+const int motorPin4 = 5;
+
+// Define the delay times for each movement
+const int forwardTime = 2000;
+const int turnTime = 500;
+
 void setup() {
-  // Set the motor pins as outputs
-  pinMode(motor1Pin1, OUTPUT);
-  pinMode(motor1Pin2, OUTPUT);
-  pinMode(motor2Pin1, OUTPUT);
-  pinMode(motor2Pin2, OUTPUT);
+  // Set the motor pins as output
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT);
+  pinMode(motorPin3, OUTPUT);
+  pinMode(motorPin4, OUTPUT);
+}
 
+void loop() {
   // Move forward for 2 seconds
-  digitalWrite(motor1Pin1, HIGH);
-  digitalWrite(motor1Pin2, LOW);
-  digitalWrite(motor2Pin1, HIGH);
-  digitalWrite(motor2Pin2, LOW);
-  analogWrite(motor1Pin2, motorSpeed);
-  analogWrite(motor2Pin2, motorSpeed);
-  delay(2000);
+  digitalWrite(motorPin1, HIGH);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, HIGH);
+  digitalWrite(motorPin4, LOW);
+  delay(forwardTime);
 
   // Turn right 90 degrees
-  digitalWrite(motor1Pin1, HIGH);
-  digitalWrite(motor1Pin2, LOW);
-  digitalWrite(motor2Pin1, LOW);
-  digitalWrite(motor2Pin2, HIGH);
-  analogWrite(motor1Pin2, motorSpeed);
-  analogWrite(motor2Pin1, motorSpeed);
-  delay(1000);
+  digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, HIGH);
+  digitalWrite(motorPin3, HIGH);
+  digitalWrite(motorPin4, LOW);
+  delay(turnTime);
 
-  // Move for 0.5 seconds
-  digitalWrite(motor1Pin1, HIGH);
-  digitalWrite(motor1Pin2, LOW);
-  digitalWrite(motor2Pin1, HIGH);
-  digitalWrite(motor2Pin2, LOW);
-  analogWrite(motor1Pin2, motorSpeed);
-  analogWrite(motor2Pin2, motorSpeed);
-  delay(500);
+  // Move for .5 seconds
+  digitalWrite(motorPin1, HIGH);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, HIGH);
+  digitalWrite(motorPin4, LOW);
+  delay(turnTime);
 
   // Turn right 90 degrees
-  digitalWrite(motor1Pin1, HIGH);
-  digitalWrite(motor1Pin2, LOW);
-  digitalWrite(motor2Pin1, LOW);
-  digitalWrite(motor2Pin2, HIGH);
-  analogWrite(motor1Pin2, motorSpeed);
-  analogWrite(motor2Pin1, motorSpeed);
-  delay(1000);
+  digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, HIGH);
+  digitalWrite(motorPin3, HIGH);
+  digitalWrite(motorPin4, LOW);
+  delay(turnTime);
 
   // Move for 2 seconds
-  digitalWrite(motor1Pin1, HIGH);
-  digitalWrite(motor1Pin2, LOW);
-  digitalWrite(motor2Pin1, HIGH);
-  digitalWrite(motor2Pin2, LOW);
-  analogWrite(motor1Pin2, motorSpeed);
-  analogWrite(motor2Pin2, motorSpeed);
-  delay(2000);
+  digitalWrite(motorPin1, HIGH);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, HIGH);
+  digitalWrite(motorPin4, LOW);
+  delay(forwardTime);
 
   // Turn left 90 degrees
-  digitalWrite(motor1Pin1, LOW);
-  digitalWrite(motor1Pin2, HIGH);
-  digitalWrite(motor2Pin1, HIGH);
-  digitalWrite(motor2Pin2, LOW);
-  analogWrite(motor1Pin1, motorSpeed);
-  analogWrite(motor2Pin2, motorSpeed);
-  delay(1000);
+  digitalWrite(motorPin1, HIGH);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, LOW);
+  digitalWrite(motorPin4, HIGH);
+  delay(turnTime);
 
-  // Move for 0.5 seconds
-  digitalWrite(motor1Pin1, HIGH);
-  digitalWrite(motor1Pin2, LOW);
-  digitalWrite(motor2Pin1, HIGH);
-  digitalWrite(motor2Pin2, LOW);
-  analogWrite(motor1Pin2, motorSpeed);
-  analogWrite(motor2Pin2, motorSpeed);
-  delay(500);
+  // Move for .5 seconds
+  digitalWrite(motorPin1, HIGH);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, HIGH);
+  digitalWrite(motorPin4, LOW);
+  delay(turnTime);
 
   // Move forward for 2 seconds
-  digitalWrite(motor1Pin1, HIGH);
-  digitalWrite(motor1Pin2, LOW);
-  digitalWrite(motor2Pin1, HIGH);
-  digitalWrite(motor2Pin2, LOW);
-  analogWrite(motor1Pin2, motorSpeed);
-  analogWrite(motor2Pin
+  digitalWrite(motorPin1, HIGH);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, HIGH);
+  digitalWrite(motorPin4, LOW);
+  delay(forwardTime);
+
+  // Stop the motors
+  digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, LOW);
+  digitalWrite(motorPin4, LOW);
+}
